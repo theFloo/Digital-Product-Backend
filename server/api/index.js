052d4d533b8619f06ab526f9cb73c11a3384da27
Talib mirza <mirzatalib04@gmail.com>
2025-11-09 18:50:39 +0530

    remove cors

--- server/api/index.js
+++ server/api/index.js
@@ -11,13 +11,13 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
-    credentials: true,
-    exposedHeaders: ["Content-Disposition"], // allow frontend to read filename header
-  })
-)
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:3000",
+//     credentials: true,
+//     exposedHeaders: ["Content-Disposition"], // allow frontend to read filename header
+//   })
+// )
 app.use(express.json({ limit: "10mb" }));
 app.set("trust proxy", 1);
 
